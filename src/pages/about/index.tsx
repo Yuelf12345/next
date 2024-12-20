5a6eb41b68d1f606569291ab4161fe45ef0fa41f
Yuelf12345 <2794127100@qq.com>
2024-12-20 16:53:10 +0800

    chore: 静态资源

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -4,9 +4,11 @@ import { useRouter } from "next/router";
 import Style from "./index.module.css";
 export default function About() {
   const router = useRouter();
-
+  const avatar = "/static/avatar.png";
   return (
     <div className={Style.container}>
+      <p>about page</p>
+      <img src={avatar} width={100} height={100} alt="avatar" />
       <Link href="/about/1">
         <p>here to 1</p>
       </Link>
@@ -19,4 +21,4 @@ export default function About() {
       <button onClick={() => router.push("/about/3")}>here to 3</button>
     </div>
   );
-};
+}
